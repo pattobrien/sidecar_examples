55429e505de62141dab5fe8bfb78afbabe44362c
pattobrien <12474318+pattobrien@users.noreply.github.com>
2022-12-20 14:10:23 -0500

    feat: Working hello world example.

--- hello_world_rules/bin/hello_world_rules.dart
+++
@@ -1,5 +0,0 @@
-import 'package:hello_world_rules/hello_world_rules.dart' as hello_world_rules;
-
-void main(List<String> arguments) {
-  print('Hello world: ${hello_world_rules.calculate()}!');
-}
--- hello_world_rules/test/hello_world_rules_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:hello_world_rules/hello_world_rules.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
